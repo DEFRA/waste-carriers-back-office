371cf32f95c6f8f6bdbe03ad3d198b4faa69f105
irisfaraway <iris.faraway@gmail.com>
2018-07-31 16:15:27 +0100

    Fix module name and inconsistencies with front-office
    The application.rb config was using an incorrectly-named module. This appeared to be causing some oddities with environment variables.
    
    I also made a number of small tweaks to make it more consistent with the waste-carriers-front-office.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,8 @@
 
 class Ability
   include CanCan::Ability
-   def initialize(user)
+
+  def initialize(user)
     can :read, WasteCarriersEngine::Registration, account_email: user.email
     can :manage, WasteCarriersEngine::TransientRegistration, account_email: user.email
   end
