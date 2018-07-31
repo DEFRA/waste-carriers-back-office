68dcbf92572b058a8371674e2c192265c4d9d882
irisfaraway <iris.faraway@gmail.com>
2018-07-31 16:07:11 +0100

    Copy over Devise and CanCanCan bits
    Add all the config, models and tests that were removed from the engine.
    
    Temporarily using the feature/move-users-out-of-engine branch of the engine, as otherwise none of it will work.

---
+++ app/models/ability.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+   def initialize(user)
+    can :read, WasteCarriersEngine::Registration, account_email: user.email
+    can :manage, WasteCarriersEngine::TransientRegistration, account_email: user.email
+  end
+end
