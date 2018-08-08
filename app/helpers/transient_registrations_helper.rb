dfba87f4f5fd0f926da2755891fe94be5d0e67ce
irisfaraway <iris.faraway@gmail.com>
2018-08-08 15:06:03 +0100

    Display addresses and person details

--- app/helpers/transient_registrations_helper.rb
+++ app/helpers/transient_registrations_helper.rb
@@ -10,4 +10,30 @@ module TransientRegistrationsHelper
       I18n.t("waste_carriers_engine.#{@transient_registration.workflow_state}s.new.heading", default: nil) ||
       @transient_registration.workflow_state
   end
+
+  def display_registered_address
+    address_lines(@transient_registration.registered_address)
+  end
+
+  def display_contact_address
+    address_lines(@transient_registration.contact_address)
+  end
+
+  def address_lines(address)
+    lines = []
+
+    if address.present?
+      lines = [address.address_line_1,
+       address.address_line_2,
+       address.address_line_3,
+       address.address_line_4,
+       address.town_city,
+       address.postcode,
+       address.country].reject
+    else
+      lines = ["-"]
+    end
+
+    lines
+  end
 end
