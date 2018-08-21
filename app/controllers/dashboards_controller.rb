420d55cad7554c42e9feff9c6f1bc4520697c737
irisfaraway <iris.faraway@gmail.com>
2018-08-21 12:51:56 +0100

    Move more logic out of index method
    This was still looking a little bulky, so let's move some of the clutter into separate methods to keep it nice and clean.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,22 +4,29 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    set_term_and_filters
+    @transient_registrations = Kaminari.paginate_array(matching_renewals).page params[:page]
+  end
+
+  private
+
+  def set_term_and_filters
     @term = params[:term]
     @in_progress = get_filter_value(params[:in_progress])
     @pending_payment = get_filter_value(params[:pending_payment])
     @pending_conviction_check = get_filter_value(params[:pending_conviction_check])
+  end
 
+  def get_filter_value(filter_param)
+    filter_param.present? && filter_param == "true"
+  end
+
+  def matching_renewals
     service = TransientRegistrationFinderService.new(@term,
                                                      @in_progress,
                                                      @pending_payment,
                                                      @pending_conviction_check)
 
-    @transient_registrations = Kaminari.paginate_array(service.transient_registrations).page params[:page]
-  end
-
-  private
-
-  def get_filter_value(filter_param)
-    filter_param.present? && filter_param == "true"
+    service.transient_registrations
   end
 end
