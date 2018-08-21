7db53dbf808ac368bb88a526c5d88e5afa4d5fbc
irisfaraway <iris.faraway@gmail.com>
2018-08-21 12:42:34 +0100

    Move dashboard searching and filtering into a service
    As discussed in the review. This makes the controller a lot lighter.
    
    This also led to some refactoring of the tests, which cut down on the length a lot.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,41 +7,11 @@ class DashboardsController < ApplicationController
     @term = params[:term]
     @filters = get_filters(params)
 
-    @transient_registrations = Kaminari.paginate_array(find_and_prepare_transient_registrations).page params[:page]
+    service = TransientRegistrationFinderService.new(@term, @filters)
+    @transient_registrations = Kaminari.paginate_array(service.transient_registrations).page params[:page]
   end
 
   private
-
-  def find_and_prepare_transient_registrations
-    transient_registrations = WasteCarriersEngine::TransientRegistration.all.order_by("metaData.lastModified": :desc)
-
-    transient_registrations = search_results(transient_registrations)
-    transient_registrations = filter_results(transient_registrations)
-
-    transient_registrations
-  end
-
-  def search_results(transient_registrations)
-    return transient_registrations unless @term.present?
-
-    # Regex to find strings which match the term, with no surrounding characters. The search is case-insensitive.
-    exact_match_regex = /\A#{@term}\z/i
-    # Regex to find strings which include the term. The search is case-insensitive.
-    partial_match_regex = /#{@term}/i
-
-    transient_registrations.any_of({ reg_identifier: exact_match_regex },
-                                   { company_name: partial_match_regex },
-                                   { last_name: partial_match_regex },
-                                   "addresses.postcode": partial_match_regex)
-  end
-
-  def filter_results(results)
-    results = results.select { |tr| tr.renewal_application_submitted? == false } if @filters[:in_progress]
-    results = results.select { |tr| tr.pending_payment? == true } if @filters[:pending_payment]
-    results = results.select { |tr| tr.pending_manual_conviction_check? == true } if @filters[:pending_conviction_check]
-
-    results
-  end
 
   def get_filters(params)
     {
