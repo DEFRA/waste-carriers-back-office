dda2e237c181c12544298f3739f48bedc3adee76
irisfaraway <iris.faraway@gmail.com>
2018-08-30 12:12:13 +0100

    Seed different types of users
    Also modify the seed script to only run if the environment is not production or we specify a particular env var.
    
    They currently all have the same role – this will change.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
-User.find_or_create_by(
-  email: "bo-user@wcr.gov.uk",
-  password: ENV["WCRS_DEFAULT_PASSWORD"] || "Secret123",
-  confirmed_at: Time.new(2015, 1, 1)
-)
+def find_or_create_user(email)
+  User.find_or_create_by(
+    email: email,
+    password: ENV["WCRS_DEFAULT_PASSWORD"] || "Secret123",
+    confirmed_at: Time.new(2015, 1, 1)
+  )
+end
+
+def seed_users
+  # Agency super
+  find_or_create_user("bo-agency-super@wcr.gov.uk")
+  # Finance super
+  find_or_create_user("bo-finance-super@wcr.gov.uk")
+
+  # Standard agency user
+  find_or_create_user("bo-user@wcr.gov.uk")
+  # Agency refund payment user
+  find_or_create_user("bo-user-with-refund@wcr.gov.uk")
+  # Finance basic user
+  find_or_create_user("bo-finance-user@wcr.gov.uk")
+  # Finance admin user
+  find_or_create_user("bo-finance-admin@wcr.gov.uk")
+end
+
+# Only seed if not running in production or we specifically require it, eg. for Heroku
+seed_users if !Rails.env.production? || ENV["WCR_ALLOW_SEED"]
