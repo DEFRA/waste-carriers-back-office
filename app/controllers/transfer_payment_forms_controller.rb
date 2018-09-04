0b29938e19341ad2c9db8fb7b8c8e8a9bbdfa6c0
irisfaraway <iris.faraway@gmail.com>
2018-09-04 17:11:51 +0100

    Only allow finance users to enter a bank transfer payment
    All other users who try to access this page should be redirected to an error.

--- app/controllers/transfer_payment_forms_controller.rb
+++ app/controllers/transfer_payment_forms_controller.rb
@@ -2,7 +2,10 @@
 
 class TransferPaymentFormsController < AdminFormsController
   def new
-    super(TransferPaymentForm, "transfer_payment_form", params[:transient_registration_reg_identifier])
+    super(TransferPaymentForm,
+          "transfer_payment_form",
+          params[:transient_registration_reg_identifier],
+          :authorize_action)
   end
 
   def create
@@ -10,6 +13,11 @@ class TransferPaymentFormsController < AdminFormsController
 
     return unless super(TransferPaymentForm,
                         "transfer_payment_form",
-                        params[:transfer_payment_form][:reg_identifier])
+                        params[:transfer_payment_form][:reg_identifier],
+                        :authorize_action)
+  end
+
+  def authorize_action(transient_registration)
+    authorize! :record_electronic_transfer_payment, transient_registration
   end
 end
--- spec/requests/transfer_payment_forms_spec.rb
+++ spec/requests/transfer_payment_forms_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "TransferPaymentForms", type: :request do
 
   describe "GET /bo/transient-registrations/:reg_identifier/payments/transfer" do
     context "when a valid user is signed in" do
-      let(:user) { create(:user) }
+      let(:user) { create(:user, :finance) }
       before(:each) do
         sign_in(user)
       end
@@ -27,25 +27,37 @@ RSpec.describe "TransferPaymentForms", type: :request do
         expect(response.body).to include(transient_registration.reg_identifier)
       end
     end
-  end
 
-  describe "POST /bo/transient-registrations/:reg_identifier/payments/transfer" do
-    context "when a valid user is signed in" do
-      let(:user) { create(:user) }
+    context "when a non-finance user is signed in" do
+      let(:user) { create(:user, :agency) }
       before(:each) do
         sign_in(user)
       end
 
-      let(:params) do
-        {
-          reg_identifier: transient_registration.reg_identifier,
-          amount: "100",
-          comment: "foo",
-          registration_reference: "foo",
-          date_received_day: "1",
-          date_received_month: "1",
-          date_received_year: "2018"
-        }
+      it "redirects to the permissions error page" do
+        get "/bo/transient-registrations/#{transient_registration.reg_identifier}/payments/transfer"
+        expect(response).to redirect_to("/bo/permission")
+      end
+    end
+  end
+
+  describe "POST /bo/transient-registrations/:reg_identifier/payments/transfer" do
+    let(:params) do
+      {
+        reg_identifier: transient_registration.reg_identifier,
+        amount: "100",
+        comment: "foo",
+        registration_reference: "foo",
+        date_received_day: "1",
+        date_received_month: "1",
+        date_received_year: "2018"
+      }
+    end
+
+    context "when a valid user is signed in" do
+      let(:user) { create(:user, :finance) }
+      before(:each) do
+        sign_in(user)
       end
 
       it "redirects to the transient_registration page" do
@@ -77,6 +89,24 @@ RSpec.describe "TransferPaymentForms", type: :request do
           post "/bo/transient-registrations/#{transient_registration.reg_identifier}/payments/transfer", transfer_payment_form: params
           expect(transient_registration.reload.finance_details.payments.count).to eq(old_payments_count)
         end
+      end
+    end
+
+    context "when a non-finance user is signed in" do
+      let(:user) { create(:user, :agency) }
+      before(:each) do
+        sign_in(user)
+      end
+
+      it "redirects to the permissions error page" do
+        post "/bo/transient-registrations/#{transient_registration.reg_identifier}/payments/transfer", transfer_payment_form: params
+        expect(response).to redirect_to("/bo/permission")
+      end
+
+      it "does not create a new payment" do
+        old_payments_count = transient_registration.finance_details.payments.count
+        post "/bo/transient-registrations/#{transient_registration.reg_identifier}/payments/transfer", transfer_payment_form: params
+        expect(transient_registration.reload.finance_details.payments.count).to eq(old_payments_count)
       end
     end
   end
