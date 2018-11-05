f680051bdced712381794a7b374d0df6432ce745
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-11-05 23:29:07 +0000

    Refactor Db::AnonymiseEmail
    Having moved the logic out of the rake task into a class, realised that there was no need to be passing the counts and paging hash between methods; we could just update it directly.
    
    Also decided anything to do with displaying output regards the task should be handled in the task hence the move of the puts statements.
    
    Finally had a go at reducing the Cognitive Complexity CodeClimate highlighted.

--- lib/db/anonymise_email.rb
+++ lib/db/anonymise_email.rb
@@ -4,24 +4,36 @@ require "db/db"
 
 module Db
   class AnonymiseEmail
+
+    attr_reader :counts
+
+    def initialize
+      @counts = Db.counts(Db.users_collection)
+      @counts[:id_increment] = 1
+
+      @paging = Db.paging(@counts[:total], 100)
+    end
+
     def anonymise
-      counts = Db.counts(Db.users_collection)
-      counts[:id_increment] = 1
-
-      paging = Db.paging(counts[:total], 100)
-
-      puts "Anonymising all emails for #{counts[:total]} users"
-      puts "-------------------------"
-
-      counts = page_through_users(paging, counts)
-
-      puts "-------------------------"
-      puts "Final results"
-      puts "Errors: #{counts[:errored]}"
-      puts "Updated: #{counts[:processed]}"
+      while @paging[:page_number] <= @paging[:num_of_pages]
+        Db.paged_users(@paging).each do |user|
+          result = anonymise_email(
+            user["email"],
+            "user#{@counts[:id_increment]}@example.com"
+          )
+          update_counts(result)
+        end
+        @paging[:page_number] += 1
+      end
     end
 
     private
+
+    def update_counts(result)
+      @counts[:id_increment] += 1
+      @counts[:processed] += 1 if result
+      @counts[:errored] += 1 unless result
+    end
 
     def anonymise_email(old_email, new_email)
       regs = update_registrations(old_email, new_email)
@@ -57,23 +69,6 @@ module Db
                  .find(email: old_email)
                  .update_one("$set": { email: new_email })
       result.n
-    end
-
-    def page_through_users(paging, counts)
-      while paging[:page_number] <= paging[:num_of_pages]
-        users = Db.paged_users(paging)
-        users.each do |user|
-          result = anonymise_email(
-            user["email"],
-            "user#{counts[:id_increment]}@example.com"
-          )
-          counts[:id_increment] += 1
-          counts[:processed] += 1 if result
-          counts[:errored] += 1 unless result
-        end
-        paging[:page_number] += 1
-      end
-      counts
     end
   end
 end
--- lib/tasks/anonymise.rake
+++ lib/tasks/anonymise.rake
@@ -7,7 +7,16 @@ namespace :db do
     desc "Anonymise all account and contact email addresses"
     task email: :environment do
       anonymiser = Db::AnonymiseEmail.new
+
+      puts "Anonymising all emails for #{anonymiser.counts[:total]} users"
+      puts "-------------------------"
+
       anonymiser.anonymise
+
+      puts "-------------------------"
+      puts "Final results"
+      puts "Errors: #{anonymiser.counts[:errored]}"
+      puts "Updated: #{anonymiser.counts[:processed]}"
     end
   end
 end
