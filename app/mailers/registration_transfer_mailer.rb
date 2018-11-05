5cb71a2dfc455884e5d0f7c189993b90cf84a57a
irisfaraway <iris.faraway@gmail.com>
2018-11-05 18:08:01 +0000

    Reduce duplication in mailer, add tests

--- app/mailers/registration_transfer_mailer.rb
+++ app/mailers/registration_transfer_mailer.rb
@@ -4,24 +4,21 @@ class RegistrationTransferMailer < ActionMailer::Base
   helper "waste_carriers_engine/mailer"
 
   def transfer_to_existing_account_email(registration)
-    @registration = registration
-
-    to_address = @registration.account_email
-    from_address = "#{Rails.configuration.email_service_name} <#{Rails.configuration.email_service_email}>"
-    subject = I18n.t(".registration_transfer_mailer.transfer_to_existing_account_email.subject",
-                     reg_identifier: @registration.reg_identifier)
-
-    mail(to: to_address,
-         from: from_address,
-         subject: subject)
+    send_email(registration, ".registration_transfer_mailer.transfer_to_existing_account_email.subject")
   end
 
   def transfer_to_new_account_email(registration)
+    send_email(registration, ".registration_transfer_mailer.transfer_to_new_account_email.subject")
+  end
+
+  private
+
+  def send_email(registration, subject_text)
     @registration = registration
 
     to_address = @registration.account_email
     from_address = "#{Rails.configuration.email_service_name} <#{Rails.configuration.email_service_email}>"
-    subject = I18n.t(".registration_transfer_mailer.transfer_to_new_account_email.subject",
+    subject = I18n.t(subject_text,
                      reg_identifier: @registration.reg_identifier)
 
     mail(to: to_address,
