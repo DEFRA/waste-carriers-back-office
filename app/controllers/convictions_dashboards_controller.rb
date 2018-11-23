77bca2f10f6e31c00610dcb8f6fd55613dda4267
irisfaraway <iris.faraway@gmail.com>
2018-11-23 09:21:35 +0000

    Display renewals requiring checks on conviction dashboard
    List all transient_registrations which have pending conviction_sign_offs on the dashboard, and link to the conviction pages.

--- app/controllers/convictions_dashboards_controller.rb
+++ app/controllers/convictions_dashboards_controller.rb
@@ -3,5 +3,7 @@
 class ConvictionsDashboardsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @transient_registrations = WasteCarriersEngine::TransientRegistration.pending_approval.page params[:page]
+  end
 end
--- spec/requests/convictions_dashboards_spec.rb
+++ spec/requests/convictions_dashboards_spec.rb
@@ -19,6 +19,22 @@ RSpec.describe "ConvictionsDashboards", type: :request do
         get "/bo/convictions"
         expect(response).to have_http_status(200)
       end
+
+      it "links to renewals which require a convictions check" do
+        last_modified_renewal = create(:transient_registration, :requires_conviction_check)
+        link_to_renewal = transient_registration_path(last_modified_renewal.reg_identifier)
+
+        get "/bo/convictions"
+        expect(response.body).to include(link_to_renewal)
+      end
+
+      it "does not link to renewals which don't require a convictions check" do
+        last_modified_renewal = create(:transient_registration)
+        link_to_renewal = transient_registration_path(last_modified_renewal.reg_identifier)
+
+        get "/bo/convictions"
+        expect(response.body).to_not include(link_to_renewal)
+      end
     end
 
     context "when a user is not signed in" do
