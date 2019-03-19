c475ae076aea984f1f3d480102b2be1b8e91e3ed
irisfaraway <iris.faraway@gmail.com>
2019-03-19 15:25:53 +0000

    Remove redundant .freeze

--- config/application.rb
+++ config/application.rb
@@ -94,7 +94,7 @@ module WasteCarriersBackOffice
     config.metadata_route = "ASSISTED_DIGITAL"
 
     # Version info
-    config.application_version = "0.0.1".freeze
+    config.application_version = "0.0.1"
     config.application_name = "waste-carriers-back-office"
     config.git_repository_url = "https://github.com/DEFRA/#{config.application_name}"
     config.generators do |g|
