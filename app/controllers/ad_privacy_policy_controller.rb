a69e588a3210eb0a67f2c79cedb4814651f33a80
cintamani <cintamani.puddu@gmail.com>
2019-07-25 12:29:02 +0100

    Add authentication to AdPrivacyPolicy controller

--- app/controllers/ad_privacy_policy_controller.rb
+++ app/controllers/ad_privacy_policy_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AdPrivacyPolicyController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @reg_identifier = params[:reg_identifier]
   end
