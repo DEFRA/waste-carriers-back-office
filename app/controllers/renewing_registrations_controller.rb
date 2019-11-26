f865097fc7fa42233e282105f9f491811bec6f31
cintamani <cintamani.puddu@gmail.com>
2019-11-26 11:48:49 +0000

    Redirects after a renewing is confirmed
    From: https://eaflood.atlassian.net/browse/RUBY-782
    
    When confirming a payment or approving convictions, a renewing registration might get confirmed and evolve to a registration. In that case, the details page of the renewing registration will not exist any more. Historically, we used to just redirect the user to the main dashboard, but during one of the many refactoring we have killed that functionality, and the test suite didn't highlight the issue due to having different mongoId settings than other environments. Hence, I have also killed those special settings as part of this story.
    Now, if a user gets to the renewal details page with a renewal id that no longer exist, we will check if we have a registration matching that id and we will redierct the user there. If there is not even a registration matching the given id, we will redirect to the dashboard as we were historically doing.

--- app/controllers/renewing_registrations_controller.rb
+++ app/controllers/renewing_registrations_controller.rb
@@ -4,9 +4,15 @@ class RenewingRegistrationsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    transient_registration = WasteCarriersEngine::RenewingRegistration.find_by(reg_identifier: params[:reg_identifier])
-
-    redirect_to bo_path unless transient_registration.present?
+    begin
+      transient_registration = WasteCarriersEngine::RenewingRegistration.find_by(reg_identifier: params[:reg_identifier])
+    rescue Mongoid::Errors::DocumentNotFound
+      if WasteCarriersEngine::Registration.where(reg_identifier: params[:reg_identifier]).any?
+        return redirect_to registration_path(params[:reg_identifier])
+      else
+        return redirect_to bo_path
+      end
+    end
 
     @transient_registration = RenewingRegistrationPresenter.new(transient_registration, view_context)
   end
