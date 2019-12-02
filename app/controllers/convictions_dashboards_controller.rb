9e1ce1c175fe0d008613402542f7717cd7d6f05a
irisfaraway <iris.faraway@gmail.com>
2019-12-02 17:42:39 +0000

    Move scope calls into separate methods

--- app/controllers/convictions_dashboards_controller.rb
+++ app/controllers/convictions_dashboards_controller.rb
@@ -4,22 +4,38 @@ class ConvictionsDashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    ordered_and_paged(WasteCarriersEngine::RenewingRegistration.submitted.convictions_possible_match)
+    ordered_and_paged(list_of_possible_matches)
   end
 
   def checks_in_progress
-    ordered_and_paged(WasteCarriersEngine::RenewingRegistration.submitted.convictions_checks_in_progress)
+    ordered_and_paged(list_of_checks_in_progress)
   end
 
   def approved
-    ordered_and_paged(WasteCarriersEngine::RenewingRegistration.submitted.convictions_approved)
+    ordered_and_paged(list_of_approved)
   end
 
   def rejected
-    ordered_and_paged(WasteCarriersEngine::RenewingRegistration.submitted.convictions_rejected)
+    ordered_and_paged(list_of_rejected)
   end
 
   private
+
+  def list_of_possible_matches
+    WasteCarriersEngine::RenewingRegistration.submitted.convictions_possible_match
+  end
+
+  def list_of_checks_in_progress
+    WasteCarriersEngine::RenewingRegistration.submitted.convictions_checks_in_progress
+  end
+
+  def list_of_approved
+    WasteCarriersEngine::RenewingRegistration.submitted.convictions_approved
+  end
+
+  def list_of_rejected
+    WasteCarriersEngine::RenewingRegistration.submitted.convictions_rejected
+  end
 
   def ordered_and_paged(transient_registrations)
     @transient_registrations = transient_registrations.order_by("metaData.lastModified": :asc)
