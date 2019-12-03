9b568c36aa0ca03ce3ba6d36c0036f5c07d7ae04
irisfaraway <iris.faraway@gmail.com>
2019-12-03 13:14:28 +0000

    Move actions display check to presenters

--- spec/presenters/registration_conviction_presenter_spec.rb
+++ spec/presenters/registration_conviction_presenter_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe RegistrationConvictionPresenter do
   let(:registration) { double(:registration) }
   let(:view_context) { double(:view_context) }
   subject { described_class.new(registration, view_context) }
+
+  describe "#display_actions?" do
+    it "returns false" do
+      expect(subject.display_actions?).to eq(false)
+    end
+  end
 end
