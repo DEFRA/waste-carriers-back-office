0b2e8625f31ce4d165a873658c545abb8303e12e
irisfaraway <iris.faraway@gmail.com>
2019-12-03 12:01:05 +0000

    Rename instance var to be more generic

--- app/controllers/convictions_controller.rb
+++ app/controllers/convictions_controller.rb
@@ -4,19 +4,19 @@ class ConvictionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    find_transient_registration(params[:transient_registration_reg_identifier])
+    find_resource(params[:transient_registration_reg_identifier])
   end
 
   def begin_checks
-    find_transient_registration(params[:reg_identifier])
-    @transient_registration.conviction_sign_offs.first.begin_checks!
+    find_resource(params[:reg_identifier])
+    @resource.conviction_sign_offs.first.begin_checks!
 
     redirect_to convictions_path
   end
 
   private
 
-  def find_transient_registration(reg_identifier)
-    @transient_registration = WasteCarriersEngine::RenewingRegistration.where(reg_identifier: reg_identifier).first
+  def find_resource(reg_identifier)
+    @resource = WasteCarriersEngine::RenewingRegistration.where(reg_identifier: reg_identifier).first
   end
 end
