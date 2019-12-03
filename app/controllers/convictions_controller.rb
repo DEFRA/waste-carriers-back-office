62d3072ae3c01239c60fd29f232a2f39af58bf30
irisfaraway <iris.faraway@gmail.com>
2019-12-03 13:02:14 +0000

    Allow @resource to be a registration

--- app/controllers/convictions_controller.rb
+++ app/controllers/convictions_controller.rb
@@ -4,11 +4,11 @@ class ConvictionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    find_resource(params[:transient_registration_reg_identifier])
+    find_resource(params)
   end
 
   def begin_checks
-    find_resource(params[:reg_identifier])
+    find_resource(find_renewing_registration(params[:reg_identifier]))
     @resource.conviction_sign_offs.first.begin_checks!
 
     redirect_to convictions_path
@@ -16,7 +16,19 @@ class ConvictionsController < ApplicationController
 
   private
 
-  def find_resource(reg_identifier)
-    @resource = WasteCarriersEngine::RenewingRegistration.where(reg_identifier: reg_identifier).first
+  def find_resource(params)
+    if params[:registration_reg_identifier].present?
+      find_registration(params[:registration_reg_identifier])
+    elsif params[:transient_registration_reg_identifier].present?
+      find_renewing_registration(params[:transient_registration_reg_identifier])
+    end
+  end
+
+  def find_registration(reg_identifier)
+    @resource = WasteCarriersEngine::Registration.find_by(reg_identifier: reg_identifier)
+  end
+
+  def find_renewing_registration(reg_identifier)
+    @resource = WasteCarriersEngine::RenewingRegistration.find_by(reg_identifier: reg_identifier)
   end
 end
