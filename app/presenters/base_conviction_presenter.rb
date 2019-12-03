e3e0c3325f5bcf9c2acdbdcc6a60b1f59ad113be
irisfaraway <iris.faraway@gmail.com>
2019-12-03 13:28:40 +0000

    Move status message to presenter

--- app/presenters/base_conviction_presenter.rb
+++ app/presenters/base_conviction_presenter.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class BaseConvictionPresenter < WasteCarriersEngine::BasePresenter
+  def conviction_status_message
+    if conviction_sign_offs.first.present?
+      I18n.t(".convictions.index.status.#{conviction_sign_offs.first.workflow_state}")
+    else
+      I18n.t(".convictions.index.status.not_required")
+    end
+  end
 end
--- spec/presenters/base_conviction_presenter_spec.rb
+++ spec/presenters/base_conviction_presenter_spec.rb
@@ -3,7 +3,33 @@
 require "rails_helper"
 
 RSpec.describe BaseConvictionPresenter do
-  let(:registration) { double(:registration) }
+  let(:conviction_sign_off) { double(:conviction_sign_off, workflow_state: "possible_match") }
+  let(:conviction_sign_offs) { [conviction_sign_off] }
+  let(:registration) do
+    double(:registration,
+           conviction_sign_offs: conviction_sign_offs)
+  end
+
   let(:view_context) { double(:view_context) }
   subject { described_class.new(registration, view_context) }
+
+  describe "#conviction_status_message" do
+    context "when a conviction_sign_off is present" do
+      it "returns the correct message" do
+        message = "This renewal may have matching or declared convictions and requires an initial review."
+
+        expect(subject.conviction_status_message).to eq(message)
+      end
+    end
+
+    context "when a conviction_sign_off is not present" do
+      let(:conviction_sign_offs) { [] }
+
+      it "returns the correct message" do
+        message = "This renewal does not require a conviction check before it can be approved."
+
+        expect(subject.conviction_status_message).to eq(message)
+      end
+    end
+  end
 end
