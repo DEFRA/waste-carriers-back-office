ae4256d98e2cd54ebafd3f6421e7f16017e80241
irisfaraway <iris.faraway@gmail.com>
2019-12-19 16:26:27 +0000

    Users can only activate or deactivate based on type
    Agency supers can only activate or deactivate agency users. Finance supers can only activate or deactivate finance users.
    
    Otherwise, redirect to a permissions error.

--- app/controllers/user_activations_controller.rb
+++ app/controllers/user_activations_controller.rb
@@ -1,24 +1,30 @@
 # frozen_string_literal: true
 
 class UserActivationsController < ApplicationController
+  include UsersHelper
+
   def activate_form
     assign_user(params[:id])
+    check_activation_permissions
     redirect_to users_url if @user.active?
   end
 
   def deactivate_form
     assign_user(params[:id])
+    check_activation_permissions
     redirect_to users_url unless @user.active?
   end
 
   def activate
     assign_user(params[:id])
+    check_activation_permissions
     @user.activate! unless @user.active?
     redirect_to users_url
   end
 
   def deactivate
     assign_user(params[:id])
+    check_activation_permissions
     @user.deactivate! if @user.active?
     redirect_to users_url
   end
@@ -27,5 +33,11 @@ class UserActivationsController < ApplicationController
 
   def assign_user(id)
     @user = User.find(id)
+  end
+
+  def check_activation_permissions
+    return if agency_with_permission?(@user, current_user) || finance_with_permission?(@user, current_user)
+
+    raise CanCan::AccessDenied
   end
 end
--- spec/requests/user_activations_spec.rb
+++ spec/requests/user_activations_spec.rb
@@ -3,27 +3,40 @@
 require "rails_helper"
 
 RSpec.describe "User Activations", type: :request do
-  let(:active_user) { create(:user, :agency) }
-  let(:inactive_user) { create(:user, :agency, :inactive) }
+  let(:current_user_role) { :agency_super }
+  let(:subject_user_role) { :agency }
+  let(:active_user) { create(:user, role: subject_user_role) }
+  let(:inactive_user) { create(:user, :inactive, role: subject_user_role) }
 
   describe "GET /bo/users/activate/:id" do
     context "when a super user is signed in" do
-      let(:user) { create(:user, :agency_super) }
+      let(:user) { create(:user, role: current_user_role) }
       before(:each) do
         sign_in(user)
       end
 
-      context "when the user to be activated is inactive" do
-        it "renders the activate_form template" do
-          get "/bo/users/activate/#{inactive_user.id}"
-          expect(response).to render_template(:activate_form)
+      context "when the current user has permission to activate the user" do
+        context "when the user to be activated is inactive" do
+          it "renders the activate_form template" do
+            get "/bo/users/activate/#{inactive_user.id}"
+            expect(response).to render_template(:activate_form)
+          end
+        end
+
+        context "when the user to be activated is already active" do
+          it "redirects to the user list" do
+            get "/bo/users/activate/#{active_user.id}"
+            expect(response).to redirect_to(users_path)
+          end
         end
       end
 
-      context "when the user to be activated is already active" do
-        it "redirects to the user list" do
-          get "/bo/users/activate/#{active_user.id}"
-          expect(response).to redirect_to(users_path)
+      context "when the current user does not have permission to activate the user" do
+        let(:current_user_role) { :finance_super }
+
+        it "redirects to a permission error" do
+          get "/bo/users/activate/#{inactive_user.id}"
+          expect(response).to redirect_to("/bo/pages/permission")
         end
       end
     end
@@ -31,22 +44,36 @@ RSpec.describe "User Activations", type: :request do
 
   describe "GET /bo/users/deactivate/:id" do
     context "when a super user is signed in" do
-      let(:user) { create(:user, :agency_super) }
+      let(:user) { create(:user, role: current_user_role) }
       before(:each) do
         sign_in(user)
       end
 
-      context "when the user to be deactivated is active" do
-        it "renders the deactivate_form template" do
-          get "/bo/users/deactivate/#{active_user.id}"
-          expect(response).to render_template(:deactivate_form)
+      context "when the current user has permission to activate the user" do
+        let(:current_user_role) { :finance_super }
+        let(:subject_user_role) { :finance }
+
+        context "when the user to be deactivated is active" do
+          it "renders the deactivate_form template" do
+            get "/bo/users/deactivate/#{active_user.id}"
+            expect(response).to render_template(:deactivate_form)
+          end
+        end
+
+        context "when the user to be deactivated is already inactive" do
+          it "redirects to the user list" do
+            get "/bo/users/deactivate/#{inactive_user.id}"
+            expect(response).to redirect_to(users_path)
+          end
         end
       end
 
-      context "when the user to be deactivated is already inactive" do
-        it "redirects to the user list" do
-          get "/bo/users/deactivate/#{inactive_user.id}"
-          expect(response).to redirect_to(users_path)
+      context "when the current user does not have permission to activate the user" do
+        let(:subject_user_role) { :finance }
+
+        it "redirects to a permission error" do
+          get "/bo/users/deactivate/#{active_user.id}"
+          expect(response).to redirect_to("/bo/pages/permission")
         end
       end
     end
@@ -54,27 +81,38 @@ RSpec.describe "User Activations", type: :request do
 
   describe "POST /bo/users/activate" do
     context "when a super user is signed in" do
-      let(:user) { create(:user, :agency_super) }
+      let(:user) { create(:user, role: current_user_role) }
       before(:each) do
         sign_in(user)
       end
 
-      context "when the user to be activated is inactive" do
-        it "redirects to the user list" do
-          post "/bo/users/activate/#{inactive_user.id}"
-          expect(response).to redirect_to(users_path)
+      context "when the current user has permission to activate the user" do
+        context "when the user to be activated is inactive" do
+          it "redirects to the user list" do
+            post "/bo/users/activate/#{inactive_user.id}"
+            expect(response).to redirect_to(users_path)
+          end
+
+          it "activates the user" do
+            post "/bo/users/activate/#{inactive_user.id}"
+            expect(inactive_user.reload.active?).to eq(true)
+          end
         end
 
-        it "activates the user" do
-          post "/bo/users/activate/#{inactive_user.id}"
-          expect(inactive_user.reload.active?).to eq(true)
+        context "when the user to be activated is already active" do
+          it "redirects to the user list" do
+            post "/bo/users/activate/#{active_user.id}"
+            expect(response).to redirect_to(users_path)
+          end
         end
       end
 
-      context "when the user to be activated is already active" do
-        it "redirects to the user list" do
-          post "/bo/users/activate/#{active_user.id}"
-          expect(response).to redirect_to(users_path)
+      context "when the current user does not have permission to activate the user" do
+        let(:subject_user_role) { :finance }
+
+        it "redirects to a permission error" do
+          post "/bo/users/activate/#{inactive_user.id}"
+          expect(response).to redirect_to("/bo/pages/permission")
         end
       end
     end
@@ -82,27 +120,41 @@ RSpec.describe "User Activations", type: :request do
 
   describe "POST /bo/users/deactivate" do
     context "when a super user is signed in" do
-      let(:user) { create(:user, :agency_super) }
+      let(:user) { create(:user, role: current_user_role) }
       before(:each) do
         sign_in(user)
       end
 
-      context "when the user to be deactivated is active" do
-        it "redirects to the user list" do
-          post "/bo/users/deactivate/#{active_user.id}"
-          expect(response).to redirect_to(users_path)
+      context "when the current user has permission to activate the user" do
+        let(:current_user_role) { :finance_super }
+        let(:subject_user_role) { :finance }
+
+        context "when the user to be deactivated is active" do
+          it "redirects to the user list" do
+            post "/bo/users/deactivate/#{active_user.id}"
+            expect(response).to redirect_to(users_path)
+          end
+
+          it "deactivates the user" do
+            post "/bo/users/deactivate/#{active_user.id}"
+            expect(active_user.reload.active?).to eq(false)
+          end
         end
 
-        it "deactivates the user" do
-          post "/bo/users/deactivate/#{active_user.id}"
-          expect(active_user.reload.active?).to eq(false)
+        context "when the user to be deactivated is already inactive" do
+          it "redirects to the user list" do
+            post "/bo/users/deactivate/#{inactive_user.id}"
+            expect(response).to redirect_to(users_path)
+          end
         end
       end
 
-      context "when the user to be deactivated is already inactive" do
-        it "redirects to the user list" do
-          post "/bo/users/deactivate/#{inactive_user.id}"
-          expect(response).to redirect_to(users_path)
+      context "when the current user does not have permission to activate the user" do
+        let(:current_user_role) { :finance_super }
+
+        it "redirects to a permission error" do
+          post "/bo/users/deactivate/#{active_user.id}"
+          expect(response).to redirect_to("/bo/pages/permission")
         end
       end
     end
