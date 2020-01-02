6e5060b21a4392dbeb5fcf54abd455ff3e281e60
cintamani <cintamani.puddu@gmail.com>
2020-01-02 17:52:52 +0000

    Isolate finance details resource
    We have realised that if we use the `finance_details._id` we are able to build a controller that does not depends on either parent. The controller contains a little hack which uses the same id to fetch the correct object by querying the two possible collections.
    This refactoring have lead to cleaner code, remove of a lot of duplication and less need of helpers.

--- app/controllers/finance_details_controller.rb
+++ app/controllers/finance_details_controller.rb
@@ -4,14 +4,15 @@ class FinanceDetailsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    find_registration(params[:registration_reg_identifier])
+    find_finance_details_registration(params[:id])
 
     @finance_details = @registration.finance_details
   end
 
   private
 
-  def find_registration(reg_identifier)
-    @registration = WasteCarriersEngine::Registration.where(reg_identifier: reg_identifier).first
+  def find_finance_details_registration(id)
+    @registration = WasteCarriersEngine::Registration.where("financeDetails._id" => BSON::ObjectId(id)).first ||
+                    WasteCarriersEngine::TransientRegistration.where("financeDetails._id" => BSON::ObjectId(id)).first
   end
 end
