8bb9d253bac116764652c5b7b6ad0c62864f1aec
irisfaraway <iris.faraway@gmail.com>
2020-01-07 09:44:19 +0000

    Refactor to use before_actions and remove duplication

--- app/controllers/user_activations_controller.rb
+++ app/controllers/user_activations_controller.rb
@@ -3,36 +3,31 @@
 class UserActivationsController < ApplicationController
   include UsersHelper
 
+  before_action :assign_user
+  before_action :check_activation_permissions
+
   def activate_form
-    assign_user(params[:id])
-    check_activation_permissions
     redirect_to users_url if @user.active?
   end
 
   def deactivate_form
-    assign_user(params[:id])
-    check_activation_permissions
     redirect_to users_url unless @user.active?
   end
 
   def activate
-    assign_user(params[:id])
-    check_activation_permissions
     @user.activate! unless @user.active?
     redirect_to users_url
   end
 
   def deactivate
-    assign_user(params[:id])
-    check_activation_permissions
     @user.deactivate! if @user.active?
     redirect_to users_url
   end
 
   private
 
-  def assign_user(id)
-    @user = User.find(id)
+  def assign_user
+    @user = User.find(params[:id])
   end
 
   def check_activation_permissions
