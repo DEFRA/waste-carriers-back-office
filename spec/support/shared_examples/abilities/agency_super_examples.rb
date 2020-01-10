304bd1203500f94db8d88ea438a3bf98c0943822
cintamani <cintamani.puddu@gmail.com>
2020-01-10 11:22:36 +0000

    Remove unnecessary tests

--- spec/support/shared_examples/abilities/agency_super_examples.rb
+++ spec/support/shared_examples/abilities/agency_super_examples.rb
@@ -8,12 +8,4 @@ RSpec.shared_examples "agency_super examples" do
   it "should be able to manage agency users" do
     should be_able_to(:manage_agency_users, User)
   end
-
-  it "should be able to cease a registration" do
-    should be_able_to(:cease, WasteCarriersEngine::Registration)
-  end
-
-  it "should be able to revoke a registration" do
-    should be_able_to(:revoke, WasteCarriersEngine::Registration)
-  end
 end
