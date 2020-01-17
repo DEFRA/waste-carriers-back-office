34c34a56bf8629f2813739d5eb4d4207863f6806
irisfaraway <iris.faraway@gmail.com>
2020-01-17 16:52:17 +0000

    Parse CSV string into arrays

--- app/services/conviction_import_service.rb
+++ app/services/conviction_import_service.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require "csv"
+
 class ConvictionImportService < ::WasteCarriersEngine::BaseService
-  def run; end
+  def run(csv)
+    CSV.parse(csv, liberal_parsing: true, skip_blanks: true)
+  end
 end
--- spec/services/conviction_import_service_spec.rb
+++ spec/services/conviction_import_service_spec.rb
@@ -3,13 +3,28 @@
 require "rails_helper"
 
 RSpec.describe ConvictionImportService do
+  let(:csv) {}
   let(:run_service) do
-    ConvictionImportService.run
+    ConvictionImportService.run(csv)
   end
 
   describe "#run" do
-    it "runs" do
-      expect { run_service }.to_not raise_error
+    context "when given CSV in a string as an argument" do
+      let(:csv) do
+        %(
+Apex Limited,,11111111,ABC,99999999
+"Doe, John",01/01/1991,,DFG,
+)
+      end
+
+      it "returns an array of arrays" do
+        array_data = [
+          ["Apex Limited", nil, "11111111", "ABC", "99999999"],
+          ["Doe, John", "01/01/1991", nil, "DFG", nil]
+        ]
+
+        expect(run_service).to eq(array_data)
+      end
     end
   end
 end
