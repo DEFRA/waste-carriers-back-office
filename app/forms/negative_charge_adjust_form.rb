d58b857e59df797f9968d47d85bc7c2e978d1cb3
cintamani <cintamani.puddu@gmail.com>
2020-02-04 13:19:32 +0000

    Add negative charge adjust form, controller and views

---
+++ app/forms/negative_charge_adjust_form.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class NegativeChargeAdjustForm < WasteCarriersEngine::BaseForm
+  include CanHaveChargeAdjustAttributes
+  include CanConvertStringToCents
+
+  def submit(params)
+    # Assign the params for validation
+    self.amount = params[:amount]
+    self.reference = params[:reference]
+    self.description = params[:description]
+
+    return false unless valid?
+
+    # Negative amount as this is a negative charge adjust
+    self.amount = string_to_cents(self.amount) * -1
+  end
+end
