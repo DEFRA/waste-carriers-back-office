9fb6824a56efe66e207d19de736d86bb16bfe792
cintamani <cintamani.puddu@gmail.com>
2020-02-11 10:19:02 +0000

    Refactor Payment factory to accomodate payment types

--- spec/factories/payment.rb
+++ spec/factories/payment.rb
@@ -4,8 +4,34 @@ FactoryBot.define do
   factory :payment, class: WasteCarriersEngine::Payment do
     amount { 100 }
     date_received { Time.now }
+    order_key { SecureRandom.uuid.split("-").last }
+
+    trait :bank_transfer do
+      payment_type { WasteCarriersEngine::Payment::BANKTRANSFER }
+    end
+
+    trait :cheque do
+      payment_type { "CHEQUE" }
+    end
+
+    trait :cash do
+      payment_type { "CASH" }
+    end
+
+    trait :postal_order do
+      payment_type { "POSTALORDER" }
+    end
+
     trait :bank_transfer do
       payment_type { "BANKTRANSFER" }
+    end
+
+    trait :worldpay do
+      payment_type { "WORLDPAY" }
+    end
+
+    trait :worldpay_missed do
+      payment_type { "WORLDPAY_MISSED" }
     end
   end
 end
