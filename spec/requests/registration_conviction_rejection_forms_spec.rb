6387f2f13938a814859132a211e1b0bbec9e70ca
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-03-10 16:47:07 +0000

    Housekeeping - merge conviction reg. reject request specs
    To improve test speed we're now happy to use multiple `expect` statements for `request` specs.
    
    This does a little housekeeping in the convictions request spec to merge the valid tests.

--- spec/requests/registration_conviction_rejection_forms_spec.rb
+++ spec/requests/registration_conviction_rejection_forms_spec.rb
@@ -12,18 +12,11 @@ RSpec.describe "RegistrationConvictionRejectionForms", type: :request do
         sign_in(user)
       end
 
-      it "renders the new template" do
+      it "renders the new template, returns a 200 response, and includes the reg identifier" do
         get "/bo/registrations/#{registration.reg_identifier}/convictions/reject"
+
         expect(response).to render_template(:new)
-      end
-
-      it "returns a 200 response" do
-        get "/bo/registrations/#{registration.reg_identifier}/convictions/reject"
         expect(response).to have_http_status(200)
-      end
-
-      it "includes the reg identifier" do
-        get "/bo/registrations/#{registration.reg_identifier}/convictions/reject"
         expect(response.body).to include(registration.reg_identifier)
       end
     end
@@ -54,34 +47,17 @@ RSpec.describe "RegistrationConvictionRejectionForms", type: :request do
         }
       end
 
-      it "redirects to the convictions page" do
+      it "redirects to the convictions page, refuses the registration, and updates the revoked_reason, workflow_state, and 'confirmed_' attributes" do
         post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
+
         expect(response).to redirect_to(convictions_path)
-      end
 
-      it "updates the revoked_reason" do
-        post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
         expect(registration.reload.metaData.revoked_reason).to eq(params[:revoked_reason])
-      end
-
-      it "updates the conviction_sign_off's confirmed_at" do
-        post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
-        expect(registration.reload.conviction_sign_offs.first.confirmed_at).to be_a(DateTime)
-      end
-
-      it "updates the conviction_sign_off's confirmed_by" do
-        post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
-        expect(registration.reload.conviction_sign_offs.first.confirmed_by).to eq(user.email)
-      end
-
-      it "updates the conviction_sign_off's workflow_state" do
-        post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
-        expect(registration.reload.conviction_sign_offs.first.workflow_state).to eq("rejected")
-      end
-
-      it "refuses the registration" do
-        post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
         expect(registration.reload.metaData.status).to eq("REFUSED")
+
+        expect(registration.reload.conviction_sign_offs.first.confirmed_at).to be_a(DateTime)
+        expect(registration.reload.conviction_sign_offs.first.confirmed_by).to eq(user.email)
+        expect(registration.reload.conviction_sign_offs.first.workflow_state).to eq("rejected")
       end
 
       context "when the params are invalid" do
@@ -91,13 +67,10 @@ RSpec.describe "RegistrationConvictionRejectionForms", type: :request do
           }
         end
 
-        it "renders the new template" do
+        it "renders the new template, and does not update the revoked_reason" do
           post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
-          expect(response).to render_template(:new)
-        end
 
-        it "does not update the revoked_reason" do
-          post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
+          expect(response).to render_template(:new)
           expect(registration.reload.metaData.revoked_reason).to_not eq(params[:revoked_reason])
         end
       end
@@ -115,13 +88,10 @@ RSpec.describe "RegistrationConvictionRejectionForms", type: :request do
         }
       end
 
-      it "redirects to the permissions error page" do
+      it "redirects to the permissions error page, and does not update the revoked_reason" do
         post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
-        expect(response).to redirect_to("/bo/pages/permission")
-      end
 
-      it "does not update the revoked_reason" do
-        post "/bo/registrations/#{registration.reg_identifier}/convictions/reject", conviction_rejection_form: params
+        expect(response).to redirect_to("/bo/pages/permission")
         expect(registration.reload.metaData.revoked_reason).to_not eq(params[:revoked_reason])
       end
     end
