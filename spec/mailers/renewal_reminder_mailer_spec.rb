72d65bec18d58da348804c823c47c8fc0be1cec4
cintamani <cintamani.puddu@gmail.com>
2020-04-17 13:31:15 +0100

    Fix tests

--- spec/mailers/renewal_reminder_mailer_spec.rb
+++ spec/mailers/renewal_reminder_mailer_spec.rb
@@ -4,16 +4,16 @@ require "rails_helper"
 
 RSpec.describe RenewalReminderMailer, type: :mailer do
   describe ".first_reminder_email" do
-    let(:registration) { create(:registration) }
+    let(:registration) { create(:registration, expires_on: 3.days.from_now) }
 
     before do
       allow(Rails.configuration).to receive(:email_service_email).and_return("test@example.com")
     end
 
     it "sends a first reminder email" do
-      described_class.first_reminder_email(registration)
+      mail = described_class.first_reminder_email(registration)
 
-      expect(mail.to).to eq([registration.contact_email, registration.account_email])
+      expect(mail.to).to eq([registration.contact_email])
       expect(mail.to).to eq([registration.account_email])
       expect(mail.body.encoded).to include(registration.reg_identifier)
     end
