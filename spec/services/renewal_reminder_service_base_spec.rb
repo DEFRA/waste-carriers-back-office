6410337152668ea00e2fe00257078b7650bd8c18
cintamani <cintamani.puddu@gmail.com>
2020-04-17 11:34:30 +0100

    Apply suggestions from code review
    Co-Authored-By: Iris Faraway <iris.faraway@gmail.com>

--- spec/services/renewal_reminder_service_base_spec.rb
+++ spec/services/renewal_reminder_service_base_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe RenewalReminderServiceBase do
   describe ".run" do
     it "send emails to relevant registrations" do
       expiring = create(:registration, expires_on: 3.days.from_now)
-      not_exiring = create(:registration, expires_on: 5.days.from_now)
+      not_expiring = create(:registration, expires_on: 5.days.from_now)
       expiring_too_soon = create(:registration, expires_on: 2.days.from_now)
       ad_contact_email = create(
         :registration,
@@ -26,7 +26,7 @@ RSpec.describe RenewalReminderServiceBase do
 
       expect_any_instance_of(TestRegistrationTransferService).to receive(:send_email).with(expiring)
 
-      expect_any_instance_of(TestRegistrationTransferService).to_not receive(:send_email).with(not_exiring)
+      expect_any_instance_of(TestRegistrationTransferService).to_not receive(:send_email).with(not_expiring)
       expect_any_instance_of(TestRegistrationTransferService).to_not receive(:send_email).with(expiring_too_soon)
       expect_any_instance_of(TestRegistrationTransferService).to_not receive(:send_email).with(ad_contact_email)
       expect_any_instance_of(TestRegistrationTransferService).to_not receive(:send_email).with(empty_contact_email)
