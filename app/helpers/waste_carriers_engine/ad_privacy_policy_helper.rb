1df5bb784b2adbc107ae2e94be3e77242ebf6cb3
cintamani <cintamani.puddu@gmail.com>
2020-05-15 13:21:06 +0100

    Remove pasted method and add test coverage

--- app/helpers/waste_carriers_engine/ad_privacy_policy_helper.rb
+++ app/helpers/waste_carriers_engine/ad_privacy_policy_helper.rb
@@ -2,10 +2,6 @@
 
 module WasteCarriersEngine
   module AdPrivacyPolicyHelper
-    def renewal_finished_link(reg_identifier:)
-      main_app.renewing_registration_path(reg_identifier: reg_identifier)
-    end
-
     def destination_path
       if @reg_identifier.present?
         WasteCarriersEngine::Engine.routes.url_helpers.new_renewal_start_form_path(token: @reg_identifier)
