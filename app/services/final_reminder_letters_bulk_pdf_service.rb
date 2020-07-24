7815b3435a4cbadb5b135a12d10c9daa47237570
irisfaraway <iris.faraway@gmail.com>
2020-07-24 16:53:10 +0100

    Refactor reminder letter services to allow subclassing
    Or, more accurately, make a superclass so we can create variants for the AD letters.

--- app/services/final_reminder_letters_bulk_pdf_service.rb
+++ app/services/final_reminder_letters_bulk_pdf_service.rb
@@ -1,39 +1,18 @@
 # frozen_string_literal: true
 
-class FinalReminderLettersBulkPdfService < ::WasteCarriersEngine::BaseService
-  def run(registrations)
-    return unless registrations.any?
-
-    @registrations = registrations
-
-    ApplicationController.new.render_to_string(
-      pdf: true,
-      template: "final_reminder_letters/bulk",
-      disable_smart_shrinking: true,
-      margin: { top: "20mm", bottom: "30mm", left: "20mm", right: "20mm" },
-      page_size: "A4",
-      print_media_type: true,
-      layout: false,
-      locals: locals
-    )
-  rescue StandardError => e
-    Airbrake.notify e
-    Rails.logger.error "Generate non-AD Final reminder letters PDF bulk error:\n#{e}"
-
-    raise e
-  end
+class FinalReminderLettersBulkPdfService < ReminderLettersBulkPdfService
 
   private
 
-  def locals
-    {
-      presenters: presenters
-    }
+  def error_label
+    "final digital reminder"
   end
 
-  def presenters
-    @registrations.map do |registration|
-      FinalReminderLetterPresenter.new(registration)
-    end
+  def template
+    "final_reminder_letters/bulk"
+  end
+
+  def presenter
+    FinalReminderLetterPresenter
   end
 end
--- app/services/final_reminder_letters_export_cleaner_service.rb
+++ app/services/final_reminder_letters_export_cleaner_service.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
-class FinalReminderLettersExportCleanerService < ::WasteCarriersEngine::BaseService
-  def run(older_than)
+class FinalReminderLettersExportCleanerService < ReminderLettersExportCleanerService
+
+  private
+
+  def export
     FinalReminderLettersExport
-      .not_deleted
-      .where(
-        created_at:
-        {
-          :$lte => older_than
-        }
-      )
-      .map(&:deleted!)
-  rescue StandardError => e
-    Airbrake.notify e, older_than: older_than
-    Rails.logger.error "Failed to delete FinalReminderLettersExport older_than #{older_than}:\n#{e}"
   end
 end
