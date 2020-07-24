b250ca70f8c0abbc0438ce042b1d2c3c3972f83f
irisfaraway <iris.faraway@gmail.com>
2020-07-24 17:30:20 +0100

    Add link to AD exports to letters index page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
     # Letters
     get "/letters", to: "letters#index", as: :letters
+    resources :ad_reminder_letters_exports, only: %i[index update], path: "/ad-reminder-letters-exports"
     resources :final_reminder_letters_exports, only: %i[index update], path: "/final-reminder-letters-exports"
   end
 
