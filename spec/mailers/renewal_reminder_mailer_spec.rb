a2b8d3e869b4c64729a3c38bd4265233210396b9
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-07-27 23:56:50 +0100

    Refactor renewal_mailer spec
    Refactoring the tests in preparation for adding new ones.

--- spec/mailers/renewal_reminder_mailer_spec.rb
+++ spec/mailers/renewal_reminder_mailer_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe RenewalReminderMailer, type: :mailer do
       allow(Rails.configuration).to receive(:email_service_email).and_return("test@example.com")
     end
 
+    let(:registration) { create(:registration, :expires_soon, contact_email: contact_email) }
+
     context "when the registration's contact email is missing" do
-      let(:registration) { create(:registration, expires_on: 3.days.from_now, contact_email: nil) }
+      let(:contact_email) { nil }
 
       it "throws an error" do
         instance = RenewalReminderMailer.new
@@ -19,7 +21,7 @@ RSpec.describe RenewalReminderMailer, type: :mailer do
     end
 
     context "when the registration's contact email is present" do
-      let(:registration) { create(:registration, expires_on: 3.days.from_now) }
+      let(:contact_email) { "foo@example.com" }
 
       it "sends a first reminder email" do
         mail = described_class.first_reminder_email(registration)
@@ -33,14 +35,14 @@ RSpec.describe RenewalReminderMailer, type: :mailer do
   end
 
   describe ".second_reminder_email" do
-    let(:registration) { create(:registration, expires_on: 3.days.from_now) }
+    let(:registration) { create(:registration, :expires_soon, contact_email: contact_email) }
 
     before do
       allow(Rails.configuration).to receive(:email_service_email).and_return("test@example.com")
     end
 
     context "when the registration's contact email is missing" do
-      let(:registration) { create(:registration, expires_on: 3.days.from_now, contact_email: nil) }
+      let(:contact_email) { nil }
 
       it "throws an error" do
         instance = RenewalReminderMailer.new
@@ -50,7 +52,7 @@ RSpec.describe RenewalReminderMailer, type: :mailer do
     end
 
     context "when the registration's contact email is present" do
-      let(:registration) { create(:registration, expires_on: 3.days.from_now) }
+      let(:contact_email) { "foo@example.com" }
 
       it "sends a second reminder email" do
         mail = described_class.second_reminder_email(registration)
