a7b1967e621152d1394adc649b8a0c47e1b2e8c7
Toby Privett <toby@snaplab.co.uk>
2021-05-19 11:37:12 +0100

    Update no account_email message on registration transfer page
    - When there is no account_email, we should not display all the paragraph
    messages (see comments on #1429)

--- app/presenters/registration_transfer_presenter.rb
+++ app/presenters/registration_transfer_presenter.rb
@@ -7,16 +7,27 @@ class RegistrationTransferPresenter < WasteCarriersEngine::BasePresenter
     super
   end
 
-  def paragraph_1_message
+  def new_registration_transfer_message_lines
+    paragraph = []
     if @registration.account_email.present?
-      I18n.t(".registration_transfers.new.paragraph_1", email: @registration.account_email)
+      paragraph << to_yml(:paragraph_1, email: @registration.account_email)
+      paragraph << to_yml(:paragraph_2, reg_identifier: @registration.reg_identifier)
+      paragraph << to_yml(:paragraph_3, email: @registration.account_email)
+      paragraph << to_yml(:paragraph_4, email: @registration.account_email)
     else
-      I18n.t(".registration_transfers.new.paragraph_1_no_account_email")
+      paragraph << to_yml(:paragraph_1_no_account_email)
+      paragraph << to_yml(:paragraph_2, reg_identifier: @registration.reg_identifier)
     end
+    paragraph
   end
 
   def account_email_message
-    @registration.account_email.presence ||
-      I18n.t(".registration_transfers.new.registration_info.values.not_applicable")
+    @registration.account_email.presence || to_yml("registration_info.values.not_applicable")
+  end
+
+  private
+
+  def to_yml(key, options = {})
+    I18n.t(".registration_transfers.new.#{key}", options)
   end
 end
--- spec/presenters/registration_transfer_presenter_spec.rb
+++ spec/presenters/registration_transfer_presenter_spec.rb
@@ -3,16 +3,26 @@
 require "rails_helper"
 
 RSpec.describe RegistrationTransferPresenter do
-  let(:registration) { double(:registration, account_email: account_email) }
+  let(:registration) do
+    double(:registration, account_email: account_email, reg_identifier: "ABC1")
+  end
+
   let(:presenter) { described_class.new(registration) }
 
   context "account_email messages" do
     context "with an account_email" do
       let(:account_email) { "alice@example.com" }
 
-      it "returns the paragraph_1 message" do
-        expect(presenter.paragraph_1_message)
-          .to eq("This registration currently belongs to alice@example.com.")
+      it "returns the new_registration_transfer_message_lines" do
+        expect(presenter.new_registration_transfer_message_lines)
+          .to eq(
+            [
+              message_for(:paragraph_1, email: account_email),
+              message_for(:paragraph_2, reg_identifier: registration.reg_identifier),
+              message_for(:paragraph_3, email: account_email),
+              message_for(:paragraph_4, email: account_email)
+            ]
+          )
       end
 
       it "returns the account_email" do
@@ -24,14 +34,22 @@ RSpec.describe RegistrationTransferPresenter do
       let(:account_email) { "" }
 
       it "returns the paragraph_1_no_email message" do
-        expect(presenter.paragraph_1_message)
-          .to eq("This registration currently does not have an account associated with it.")
+        expect(presenter.new_registration_transfer_message_lines)
+          .to eq(
+            [
+              message_for(:paragraph_1_no_account_email),
+              message_for(:paragraph_2, reg_identifier: registration.reg_identifier)
+            ]
+          )
       end
 
       it "returns n/a" do
-        expect(presenter.account_email_message)
-          .to eq("n/a")
+        expect(presenter.account_email_message).to eq("n/a")
       end
+    end
+
+    def message_for(key, options = {})
+      I18n.t(".registration_transfers.new.#{key}", options)
     end
   end
 end
