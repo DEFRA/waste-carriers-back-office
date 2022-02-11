c831dac4b0848f9174439473cdeb2125d7e42d07
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-02-11 12:28:22 +0000

    Move .csv extension from file pattern to file_name

--- spec/services/reports/card_orders_export_service_spec.rb
+++ spec/services/reports/card_orders_export_service_spec.rb
@@ -5,8 +5,8 @@ require "rails_helper"
 module Reports
   RSpec.describe CardOrdersExportService do
     describe ".run" do
-      let(:file_name) { "card_orders_#{Date.today.strftime('%Y-%m-%d')}" }
-      let(:aws_file_pattern) { %r{https://.*\.s3\.eu-west-1\.amazonaws\.com/CARD_ORDERS/#{file_name}\.csv.*} }
+      let(:file_name) { "card_orders_#{Date.today.strftime('%Y-%m-%d')}.csv" }
+      let(:aws_file_pattern) { %r{https://.*\.s3\.eu-west-1\.amazonaws\.com/CARD_ORDERS/#{file_name}.*} }
       let(:end_time) { DateTime.now + 1.hour }
       let(:start_time) { end_time - 7.days }
       aws_stub = nil
