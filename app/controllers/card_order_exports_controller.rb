1bd02eb36ea2c13532532b6cee072fec10cca3ff
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-02-11 18:27:06 +0000

    Address potential unprotected redirect

--- app/controllers/card_order_exports_controller.rb
+++ app/controllers/card_order_exports_controller.rb
@@ -13,7 +13,7 @@ class CardOrderExportsController < ApplicationController
     export_log.first_visited_at = Time.now
     export_log.first_visited_by = current_user.email
     export_log.save!
-    redirect_to export_log.download_link, allow_other_host: true
+    redirect_to export_log.download_link.to_s
   end
 
   private
