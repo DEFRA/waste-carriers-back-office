1991701fec4c33a368cc6009480d74b9b7f14b60
Toby Privett <toby@snaplab.co.uk>
2022-03-17 09:37:58 +0000

    Enable support for `is_a?(klass)` on the original model
    - Unfortunately, the ActionLinksHelper uses `is_a?(model-klass)` to
    make page display decisions.
    
    - Here we enable that method to be called on the model that was passed to this
    class so we can continue to support that behaviour

--- app/presenters/search_result_presenter.rb
+++ app/presenters/search_result_presenter.rb
@@ -2,4 +2,16 @@
 
 class SearchResultPresenter < WasteCarriersEngine::BasePresenter
   include WasteCarriersEngine::CanPresentEntityDisplayName
+
+  def initialize(model)
+    @original_model = model
+    super(model)
+  end
+
+  # ActionLinksHelper uses `is_a?(model-klass)` to make page display decisions.
+  # Here we enable that method to be called on the model that was passed to this
+  # class so we can continue to support that behaviour
+  def is_a?(klass)
+    @original_model.is_a?(klass)
+  end
 end
