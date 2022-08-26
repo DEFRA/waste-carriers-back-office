0e72634f434c8ac57ce8df9a1c774c049b6bbc55
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-08-26 15:00:41 +0100

    Update finance_reports_controller to use separate downloads page

--- app/controllers/finance_reports_controller.rb
+++ app/controllers/finance_reports_controller.rb
@@ -2,27 +2,24 @@
 
 class FinanceReportsController < ApplicationController
   include CanSetFlashMessages
+  include CanListFilesOnAws
 
   before_action :authorize_user
 
   def show
-    finance_reports_service.run
-
-    send_file finance_reports_service.report_file_path, type: "text/html", disposition: "attachment"
-  rescue StandardError => e
-    Airbrake.notify e
-    Rails.logger.error "Error generating financial reports file:\n#{e}\n#{e.backtrace}"
-    flash.now[:error] = I18n.t("download_finance_reports.messages.failure")
+    render :show, locals: { report_download_url: finance_report_download_url,
+                            report_filename: finance_report_filename }
   end
 
   private
 
-  def finance_reports_service
-    @finance_reports_service ||= Reports::FinanceReportsService.new
+  def finance_report_download_url
+    @finance_report_download_url ||= Reports::FinanceReportsAwsService.new.download_link
   end
 
-  def report_file_path
-    @report_file_path ||= finance_reports_service.report_file_path
+  def finance_report_filename
+    uri = URI.parse(finance_report_download_url)
+    File.basename(uri.path)
   end
 
   def report_file_name
