6a955ea923994f9939d967e563ca9ee8cea10264
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-09-12 13:11:28 +0100

    Resolve merge conflicts

--- spec/services/reports/finance_report_service_spec.rb
+++ spec/services/reports/finance_report_service_spec.rb
@@ -8,15 +8,65 @@ module Reports
     # Tests for the structure and content of the report are in the serializer_spec.
     describe ".run" do
 
-      subject { described_class.new }
+      context "when the report is successfully generated and uploaded to S3" do
+        let(:temp_dir) { Dir.mktmpdir }
+        let(:bucket) { double(:bucket) }
+        let(:upload_result) { double(:result) }
+        let(:list_files_result) { double(:result) }
+        let(:test_filename) { "test_file.csv" }
+        let(:s3_test_filepath) { "FINANCE_REPORTS/#{test_filename}" }
+        let(:s3_test_previous_filepath) { "FINANCE_REPORTS/a_file" }
+        let(:file_path) { File.join(temp_dir, test_filename) }
 
-      # Clean up the test /tmp area after generating the files
-      after(:each) { FileUtils.rm_rf(Pathname.new(subject.report_file_path).dirname.to_s) }
+        it "generates a report file, uploads it to AWS and deletes any previous files" do
 
-      it "generates a report file" do
-        subject.run
+          # populate test dir
+          File.open(file_path, "w")
 
-        expect(File.exist?(subject.report_file_path)).to be true
+          allow_any_instance_of(described_class).to receive(:file_name).and_return(test_filename)
+          expect(Dir).to receive(:mktmpdir).and_yield(temp_dir)
+          expect(DefraRuby::Aws).to receive(:get_bucket).and_return(bucket).at_least(:once)
+          expect(bucket).to receive(:load).and_return(upload_result)
+          expect(upload_result).to receive(:successful?).and_return(true).at_least(:once)
+          expect(bucket).to receive(:list_files).and_return(list_files_result)
+          expect(list_files_result).to receive(:successful?).and_return(true).at_least(:once)
+          expect(list_files_result).to receive(:result).and_return([s3_test_previous_filepath, s3_test_filepath]).at_least(:once)
+          expect(bucket).to receive(:delete).with(s3_test_previous_filepath)
+
+          # expect no errors
+          expect(Airbrake).to_not receive(:notify)
+          expect(Rails.logger).to_not receive(:error)
+
+          described_class.run
+        end
+      end
+
+      context "when an error happens" do
+        it "raises an error" do
+          expect(FinanceStatsService).to receive(:new).and_raise("An error!")
+
+          expect(Airbrake).to receive(:notify)
+          expect(Rails.logger).to receive(:error)
+
+          described_class.run
+        end
+      end
+    end
+
+    describe ".download_url" do
+      context "when the service has not yet been run" do
+        it "raises an exception" do
+          expect { described_class.new.download_url }.to raise_error(StandardError)
+        end
+      end
+
+      context "when the service has been run" do
+        it "returns the URL of report the file on S3" do
+          service = described_class.new
+          service.run
+
+          expect(service.download_url).to include "FINANCE_REPORTS"
+        end
       end
     end
   end
