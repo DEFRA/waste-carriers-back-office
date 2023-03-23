527247fabe8321f050ea1a41001161f1f1d86663
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-03-23 13:03:39 +0000

    email export batch size as integer

--- app/controllers/email_exports_controller.rb
+++ app/controllers/email_exports_controller.rb
@@ -21,7 +21,7 @@ class EmailExportsController < ApplicationController
     batch_size = params[:batch_size].to_i
 
     if valid_batch_size?(batch_size)
-      DeregistrationEmailExportService.run(params[:batch_size])
+      DeregistrationEmailExportService.run(batch_size)
       redirect_to new_email_exports_list_path
     else
       flash_error(I18n.t("email_exports.messages.error"),
