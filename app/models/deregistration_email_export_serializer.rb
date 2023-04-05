7d5c05753f30ce1949b53b547c81c32b149b04d6
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-04-05 18:03:26 +0100

    Revert "Fix presentation return value logic"
    This reverts commit e16be09c9ed522db6dcfc5253e1a6538cce685ea.

--- app/models/deregistration_email_export_serializer.rb
+++ app/models/deregistration_email_export_serializer.rb
@@ -51,10 +51,8 @@ class DeregistrationEmailExportSerializer < Reports::BaseCsvFileSerializer
     if ENV.fetch("WCRS_REGSDB_URI").match?(/replicaSet/)
       registration.with_session do |session|
         session.start_transaction
-        row = process_registration(registration)
+        process_registration(registration)
         session.commit_transaction
-
-        row
       end
     else
       process_registration(registration)
