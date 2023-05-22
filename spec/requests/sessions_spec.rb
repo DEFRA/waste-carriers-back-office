225c846bea81e496d0ca1a600e1b52e3fdc53fd9
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-05-22 21:07:19 +0100

    Add spec that tests the logging out of other login

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -35,6 +35,28 @@ RSpec.describe "Sessions" do
         end
       end
     end
+
+    context "when the user is already logged in from a different session" do
+      let(:user) { create(:user, current_login_token: Devise.friendly_token) }
+
+      it "signs the user out and redirects due to concurrent session" do
+        # Log in user
+        post user_session_path, params: { user: { email: user.email, password: user.password } }
+        expect(response).to redirect_to(bo_path)
+
+        # The new login token simulates the token from another session
+        new_login_token = Devise.friendly_token
+        user.update(current_login_token: new_login_token)
+
+        # Simulate the next request in which the check_concurrent_session before_action is triggered
+        get bo_path
+        expect(response).to redirect_to(new_user_session_path)
+        follow_redirect!
+
+        expect(response.body)
+          .to include("Your login credentials were used in another browser. Please sign in again to continue in this browser.")
+      end
+    end
   end
 
   describe "DELETE /bo/users/sign_out" do
