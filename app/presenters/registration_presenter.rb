9f414d5ce855e4debcccf6ba01c0b46c82c26a0c
Leonid Brujev <brujev@gmail.com>
2023-05-26 17:03:49 +0100

    refactoring : moving redistration_date methods from registration_presenter to base_registration_presenter to make them available for transient_registrations

--- app/presenters/registration_presenter.rb
+++ app/presenters/registration_presenter.rb
@@ -16,15 +16,4 @@ class RegistrationPresenter < BaseRegistrationPresenter
   def display_registration_status
     metaData.status.titleize
   end
-
-  def display_registration_date
-    metaData.dateRegistered&.to_date
-  end
-
-  def display_original_registration_date
-    return if metaData.dateRegistered.blank?
-
-    sanitize(I18n.t(".shared.registrations.company_details_panel.labels.registration_date_html",
-                    formatted_date: display_registration_date))
-  end
 end
