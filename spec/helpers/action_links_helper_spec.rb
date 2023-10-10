121eb089cde8a10ed17f9ac2fbd8baa45fb6eb2a
Leonid Brujev <brujev@gmail.com>
2023-10-10 16:41:18 +0100

    [RUBY-2667] refactoring tests

--- spec/helpers/action_links_helper_spec.rb
+++ spec/helpers/action_links_helper_spec.rb
@@ -948,44 +948,10 @@ RSpec.describe ActionLinksHelper do
   end
 
   describe "#display_transfer_link_for?" do
-    context "when the resource is not a Registration" do
-      let(:resource) { build(:renewing_registration) }
+    let(:resource) { build(:registration) }
 
-      it "returns false" do
-        expect(helper.display_transfer_link_for?(resource)).to be(false)
-      end
-    end
-
-    context "when the resource is a Registration" do
-      let(:resource) { build(:registration) }
-
-      context "when the resource has been revoked or refused" do
-        before { resource.metaData.status = %w[REVOKED REFUSED].sample }
-
-        it "returns false" do
-          expect(helper.display_transfer_link_for?(resource)).to be(false)
-        end
-      end
-
-      context "when the resource is not revoked or refused" do
-        before { resource.metaData.status = %w[ACTIVE EXPIRED PENDING].sample }
-
-        context "when the user does not have permission" do
-          before { allow(helper).to receive(:can?).with(:transfer_registration, WasteCarriersEngine::Registration).and_return(false) }
-
-          it "returns false" do
-            expect(helper.display_transfer_link_for?(resource)).to be(false)
-          end
-        end
-
-        context "when the user has permission" do
-          before { allow(helper).to receive(:can?).with(:transfer_registration, WasteCarriersEngine::Registration).and_return(true) }
-
-          it "returns true" do
-            expect(helper.display_transfer_link_for?(resource)).to be(true)
-          end
-        end
-      end
+    it "returns false" do
+      expect(helper.display_transfer_link_for?(resource)).to be(false)
     end
   end
 
