229edcabb328f0d4af8068ead56a6d2de0a75db3
Leonid Brujev <brujev@gmail.com>
2026-03-27 11:03:07 +0000

    Adding seeds convictions data

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,22 @@ end
 
 # Only seed if not running in production or we specifically require it, eg. for Heroku
 seed_users if !Rails.env.production? || ENV["WCR_ALLOW_SEED"]
+
+# rubocop:disable Rails/Output
+def seed_convictions
+  puts "Seeding convictions data..."
+
+  seeds = JSON.parse(Rails.root.join("db/seeds/convictions.json").read)
+  convictions_data = seeds["convictions"]
+
+  WasteCarriersEngine::ConvictionsCheck::Entity.delete_all
+  convictions_data.each do |data|
+    data["date_of_birth"] = Date.parse(data["date_of_birth"]) if data["date_of_birth"]
+    WasteCarriersEngine::ConvictionsCheck::Entity.create!(data.symbolize_keys)
+  end
+
+  puts "Seeded #{WasteCarriersEngine::ConvictionsCheck::Entity.count} conviction records"
+end
+# rubocop:enable Rails/Output
+
+seed_convictions if !Rails.env.production? || ENV["WCR_ALLOW_SEED"]
